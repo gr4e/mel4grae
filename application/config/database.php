6dd0279f563f2a8f1d93808764bf31ee48a92e87
Grae Buenaventura <buenaventura.grae@gmail.com>
2024-09-29 23:58:54 +0800

    db

--- application/config/database.php
+++ application/config/database.php
@@ -76,9 +76,9 @@ $query_builder = TRUE;
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'graemelwed',
+	'username' => 'dbmasteruser',
+	'password' => 'nyau5boP5DRb651SHOxLH=,?=eM%tH]%',
+	'database' => 'Database-1',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
